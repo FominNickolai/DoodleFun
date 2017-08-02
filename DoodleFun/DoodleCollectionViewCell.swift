7ea8152bff02716914339f5f7658800b1f9024b4
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-02 16:10:04 +0300

    Add UICollectionViewDelegateFlowLayout size method

---
+++ DoodleFun/DoodleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DoodleCollectionViewCell.swift
+//  DoodleFun
+//
+//  Created by Fomin Mykola on 8/2/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
--- DoodleFun/DoodleViewController.swift
+++ DoodleFun/DoodleViewController.swift
@@ -9,13 +9,55 @@
 import UIKit
 
 class DoodleViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    fileprivate var doodleImages = ["DoodleIcons-1", "DoodleIcons-2", "DoodleIcons-3","DoodleIcons-4", "DoodleIcons-5","DoodleIcons-6", "DoodleIcons-7", "DoodleIcons-8","DoodleIcons-9", "DoodleIcons-10", "DoodleIcons-11","DoodleIcons-12", "DoodleIcons-13", "DoodleIcons-14","DoodleIcons-15", "DoodleIcons-16", "DoodleIcons-17","DoodleIcons-18", "DoodleIcons-19", "DoodleIcons-20"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("\(traitCollection)")
     }
     
     
 
 }
+//MARK: - UICollectionViewDataSource
+extension DoodleViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return doodleImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! DoodleCollectionViewCell
+        
+        cell.imageView.image = UIImage(named: doodleImages[indexPath.row])
+        
+        return cell
+    }
+}
+
+//MARK: - UICollectionViewDelegate
+extension DoodleViewController: UICollectionViewDelegate {
+    
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension DoodleViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideSize = (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) ? 80.0 : 128.0
+        
+        return CGSize(width: sideSize, height: sideSize)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
